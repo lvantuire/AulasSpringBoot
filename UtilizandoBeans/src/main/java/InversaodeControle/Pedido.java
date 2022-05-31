8409a42a6a8c0c14e04f684ca467b4ceed7d729f
lvantuire <lvantuire@gmail.com>
2022-05-31 12:57:22 -0300

    Inlcuindo Aula Inversao de Controles

--- UtilizandoBeans/src/main/java/InversaodeControle/Pedido.java
+++ UtilizandoBeans/src/main/java/InversaodeControle/Pedido.java
@@ -1,2 +1,15 @@
-package InversaodeControle;public class Pedido {
+package InversaodeControle;
+
+public class Pedido {
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido();
+
+        pedido.gravar();
+    }
+
+    private EnviarEmails enviar = EnviarEmails.obterDadosEmail();
+
+    public void gravar() {
+        this.enviar.retornar("Pedido Criado!");
+    }
 }
